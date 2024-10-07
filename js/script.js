556bfece7cbc3b5561099ae8a1bf15118a01f705
Xayritdinov <rafa.chgm@gmail.com>
2024-10-07 23:33:23 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let wrapper = document.querySelector (".wrapper");
+let popUp = document.querySelector (".wrapper .popUp");
+let typ = document.querySelector (".wrapper .thankYouPage");
+let btns = document.querySelectorAll (".mainScreen__btn button");
+let formBtn = document.querySelector (".form button");
+let username = document.getElementsByName ("username")[0];
+let userphone = document.getElementsByName ("userphone")[0];
+let comments = document.getElementsByName ("comments")[0];
+
+btns.forEach (btn => {
+    btn.addEventListener ('click', showpopUp);
+})
+wrapper.addEventListener ('click', hidepopUp);
+
+function showpopUp() {
+    username.value = "";
+    userphone.value = "";
+    comments.value = "";
+    
+    wrapper.style.display = "flex";
+    popUp.style.display = "flex";  
+    typ.style.display = "none";
+}
+function hidepopUp() {
+    let elem = event.target;
+    if (elem == wrapper) {
+        wrapper.style.display = "none";
+    }
+}
+formBtn.addEventListener ("click", sendForm);
+function sendForm() {
+    event.preventDefault();    
+    if (username.value.length > 1) {
+        if (userphone.value.length == 13){
+            showTYP();
+        } else {
+            console.log ('неверный номер'); 
+        } 
+        }else {
+        console.log ('короткое имя');
+        }
+    }
+function showTYP() {
+    popUp.style.display = "none";    
+    typ.style.display = "flex";
+}
+
+
+
